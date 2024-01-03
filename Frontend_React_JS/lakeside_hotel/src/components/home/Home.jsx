d47269c3efc9e6dc2b400afe9d6ba0afe4b48360
Adebayo Adesina <adebayoadesina789@gmail.com>
2024-01-03 18:22:41 +0100

    Home carousel added with avaliable room.

--- Frontend_React_JS/lakeside_hotel/src/components/home/Home.jsx
+++ Frontend_React_JS/lakeside_hotel/src/components/home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import MainHeader from '../layout/MainHeader'
+import HotelService from '../common/HotelService'
+import Parallax from '../common/Parallax'
+import RoomCarousel from '../common/RoomCarousel'
+ '../layout/MainHeader'
 
 const Home = () => {
   return (
-    <div>
-        <h2>Welcome to the Home Page</h2>
-    </div>
+    <section>
+        <MainHeader/>
+        <section className="container">
+          <Parallax/>
+          <HotelService/>
+          <Parallax/>
+          <RoomCarousel/>
+        </section>
+    </section>
   )
 }
 
